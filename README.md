84441825872b08a6377cd23db46363706ebadc7a
jonlazarini <jlazarini@deloitte.co.uk>
2021-07-16 11:26:11 +0100

    chore(cvsb-19465): format

--- README.md
+++ README.md
@@ -1,29 +1,36 @@
 # cvs-tsk-retro-gen-init
 
 ### Prerequisites
+
 - NodeJS 8.10
 - Typescript - `npm install -g typescript`
 - Serverless - `npm install -g serverless`
 - Docker
 
 ### Installing
+
 - Install dependencies - `npm install`
 
 ### Building
+
 - Building without source maps - `npm run build`
 - Building with source maps - `npm run build:dev`
 - Building the docker containers - `npm run build:docker`
 
 ### Running
+
 - The app can be started by running `npm run start:docker`.
 
 ### Configuration
+
 The configuration file can be found under `src/config/config.yml`.
 Environment variable injection is possible with the syntax:
 `${BRANCH}`, or you can specify a default value: `${BRANCH:local}`.
 
 #### SQS
+
 SQS contains configuration for the local environment or the AWS environment. Locally, you need to provide the `localhost` region and the SQS endpoint, which in this case is the address of a docker image. `apiVersion` and `queueName` need to be provided for both environments.
+
 ```
 sqs:
   local:
@@ -63,9 +70,10 @@ https://github.com/UKHomeOffice/repo-security-scanner
 These will be run as part of prepush so please make sure you set up the git hook above so you don't accidentally introduce any new security vulnerabilities.
 
 ### Testing
-In order to test, you need to run the following:
-- `npm run test` for unit tests
 
+In order to test, you need to run the following:
+
+- `npm run test` for unit tests
 
 ### Environmental variables
 
--- src/functions/retroGenInit.ts
+++ src/functions/retroGenInit.ts
@@ -1,10 +1,10 @@
-import {Callback, Context, DynamoDBRecord, Handler} from "aws-lambda";
-import {AWSError} from "aws-sdk";
-import {Injector} from "../models/injector/Injector";
-import {SQService} from "../services/SQService";
-import {PromiseResult} from "aws-sdk/lib/request";
-import {SendMessageResult} from "aws-sdk/clients/sqs";
-import {StreamService} from "../services/StreamService";
+import { Callback, Context, DynamoDBRecord, Handler } from "aws-lambda";
+import { AWSError } from "aws-sdk";
+import { Injector } from "../models/injector/Injector";
+import { SQService } from "../services/SQService";
+import { PromiseResult } from "aws-sdk/lib/request";
+import { SendMessageResult } from "aws-sdk/clients/sqs";
+import { StreamService } from "../services/StreamService";
 
 /**
  * λ function to process a DynamoDB stream of test results into a queue for certificate generation.
@@ -12,29 +12,34 @@ import {StreamService} from "../services/StreamService";
  * @param context - λ Context
  * @param callback - callback function
  */
-const retroGenInit: Handler = async (event: any, context?: Context, callback?: Callback): Promise<void | Array<PromiseResult<SendMessageResult, AWSError>>> => {
-    if (!event) {
-        console.error("ERROR: event is not defined.");
-        return;
-    }
+const retroGenInit: Handler = async (
+  event: any,
+  context?: Context,
+  callback?: Callback
+): Promise<void | Array<PromiseResult<SendMessageResult, AWSError>>> => {
+  if (!event) {
+    console.error("ERROR: event is not defined.");
+    return;
+  }
 
-    // Convert the received event into a readable array of filtered visits
-    const records: DynamoDBRecord[] = StreamService.getVisitsStream(event);
+  // Convert the received event into a readable array of filtered visits
+  const records: DynamoDBRecord[] = StreamService.getVisitsStream(event);
 
-    // Instantiate the Simple Queue Service
-    const sqService: SQService = Injector.resolve<SQService>(SQService);
-    const sendMessagePromises: Array<Promise<PromiseResult<SendMessageResult, AWSError>>> = [];
+  // Instantiate the Simple Queue Service
+  const sqService: SQService = Injector.resolve<SQService>(SQService);
+  const sendMessagePromises: Array<
+    Promise<PromiseResult<SendMessageResult, AWSError>>
+  > = [];
 
-    // Add each record to the queue
-    records.forEach(async (record: any) => {
-        sendMessagePromises.push(sqService.sendMessage(JSON.stringify(record)));
-    });
+  // Add each record to the queue
+  records.forEach(async (record: any) => {
+    sendMessagePromises.push(sqService.sendMessage(JSON.stringify(record)));
+  });
 
-    return Promise.all(sendMessagePromises)
-    .catch((error: AWSError) => {
-        console.error(error);
-        throw error;
-    });
+  return Promise.all(sendMessagePromises).catch((error: AWSError) => {
+    console.error(error);
+    throw error;
+  });
 };
 
-export {retroGenInit};
+export { retroGenInit };
